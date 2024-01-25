aaa706395f3d4e97c509683e86cdb51c314601d9
DEOKJAE KWON <juintin@kakao.com>
2024-01-25 21:14:08 +0900

    feat: add userLogin logic

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,19 @@
-var fakeLogin = require('./passport/fake_login');
+ var fakeLogin = require('./passport/fake_login');
+const usersLogin = require('./passport/users_login');
 
 module.exports = (app, passport) => {
   // 세션 처리 - 로그인에 성공했을 경우 딱 한번 호출되어 사용자의 식별자를 session에 저장
   passport.serializeUser((user, done) => {
-    console.log('serializeUser', user);
-    done(null, user.username);
+    console.log('serializeUser', user.email);
+    done(null, user);
   });
 
   // 세션 처리 - 로그인 후 페이지 방문마다 사용자의 실제 데이터 주입
   passport.deserializeUser((user, done) => {
-    console.log('deserializeUser', user);
+    console.log('deserializeUser', user.email);
     done(null, user);
   });
 
   // 인증 방식
-  passport.use('local', fakeLogin);
+  passport.use('local', usersLogin);
 };
---
+++ config/passport/users_login.js
@@ -0,0 +1,24 @@
+const LocalStrategy = require('passport-local').Strategy;
+const axios = require('axios');
+
+module.exports = new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+    // HTTP 요청을 통해 사용자 정보를 얻어옴
+    const response = await axios.get(`http://localhost:8080/user/${email}`);
+    
+    // 응답에서 필요한 정보를 추출
+    const user = response.data;
+    
+    if (email === user.email) {
+      if (password === user.pwd) {
+        return done(null, user);
+      } else {
+        return done(null, false, req.flash('loginMessage', 'password incorrect'));
+      }
+    } else {
+      return done(null, false, req.flash('loginMessage', 'email incorrect'));
+    }
+});
